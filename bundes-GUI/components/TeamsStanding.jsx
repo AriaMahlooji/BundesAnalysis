dac2a0e256351ee5671dcb2fd7e53d191045ef84
AriaMahlouji <ariamahlooji.de@gmail.com>
2023-08-31 09:26:04 +0200

    Adding teams logo to standing table

--- bundes-GUI/components/TeamsStanding.jsx
+++ bundes-GUI/components/TeamsStanding.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import data from "../data/data";
+import Image from "next/image";
 
-const TeamsStanding = ({standingInfo}) => {
-  if(!standingInfo)
-  {
+const TeamsStanding = ({ standingInfo }) => {
+  if (!standingInfo) {
     return null;
   }
-  return(
+  return (
     <div className="bg-gray-200 min-h-screen">
       <div className="">
         <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
@@ -23,9 +23,7 @@ const TeamsStanding = ({standingInfo}) => {
               <div className=" md:col-span-2 text-sm flex justify-center">
                 Goals
               </div>
-              <div className=" text-sm flex justify-center">
-                Pts
-              </div>
+              <div className=" text-sm flex justify-center">Pts</div>
             </div>
           </div>
           <hr></hr>
@@ -34,12 +32,31 @@ const TeamsStanding = ({standingInfo}) => {
               <li>
                 <div className="my-3 p-2 grid md:grid-cols-12 rounded-lg bg-gray-100">
                   <div className="text-sm flex items-center md:col-span-1 justify-center ">
-                    <span className={`flex items-center justify-center rounded-lg w-5 ${standingInfo.indexOf(info)+1===1? "bg-golden": standingInfo.indexOf(info)+1===2? "bg-silver":standingInfo.indexOf(info)+1===3?" bg-bronze":""}`}>
-                      {standingInfo.indexOf(info)+1}
+                    <span
+                      className={`flex items-center justify-center rounded-lg w-5 ${
+                        standingInfo.indexOf(info) + 1 === 1
+                          ? "bg-golden"
+                          : standingInfo.indexOf(info) + 1 === 2
+                          ? "bg-silver"
+                          : standingInfo.indexOf(info) + 1 === 3
+                          ? " bg-bronze"
+                          : ""
+                      }`}
+                    >
+                      {standingInfo.indexOf(info) + 1}
                     </span>
                   </div>
                   <div className="text-sm flex md:col-span-7  ">
-                    {info.team.name}
+                    <div className="flex items-center">
+                      <Image
+                        className="rounded-[50%]"
+                        src={`data:image/png;base64,${info.teamLogoUrl}`}
+                        width="30"
+                        height="30"
+                        alt="user"
+                      ></Image>
+                      <div className="ml-3">{info.team.name}</div>
+                    </div>
                   </div>
                   <div className="grid md:grid-cols-7 md:col-span-4">
                     <div className=" text-sm flex items-center justify-center">
@@ -49,14 +66,19 @@ const TeamsStanding = ({standingInfo}) => {
                       {info.wonCount}
                     </div>
                     <div className=" text-sm flex items-center justify-center ">
-                    {info.lostCount}
+                      {info.lostCount}
                     </div>
                     <div className="text-sm flex items-center justify-center">
-                    {info.drawCount}
+                      {info.drawCount}
                     </div>
                     <div className=" md:col-span-2 flex justify-center">
-                      <div className="text-sm flex items-center rounded bg-green-200 mr-1">{info.scoredGoalsCount} </div>
-                      <div className="text-sm flex items-center rounded bg-red-200"> {info.receivedGoalsCount}</div>
+                      <div className="text-sm flex items-center rounded bg-green-200 mr-1">
+                        {info.scoredGoalsCount}{" "}
+                      </div>
+                      <div className="text-sm flex items-center rounded bg-red-200">
+                        {" "}
+                        {info.receivedGoalsCount}
+                      </div>
                     </div>
                     <div className="text-sm flex items-center justify-center">
                       {info.points}
@@ -68,7 +90,8 @@ const TeamsStanding = ({standingInfo}) => {
           </ul>
         </div>
       </div>
-    </div>);
+    </div>
+  );
 };
 
 export default TeamsStanding;
